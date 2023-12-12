1c31ffb28101e73d1508d29da2e4aefe8cb59ece
ozkostay <ozkostay@mail.ru>
2023-12-12 21:31:45 +0500

    red border on IMG

--- frontend/src/components/hotels/AddHotelPics.jsx
+++ frontend/src/components/hotels/AddHotelPics.jsx
@@ -1,27 +1,84 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function AddHotelPics() {
   // const { user } = useSelector((state) => state.crUser);
-  const [pics, setPics] = useState([]);
+  const [picsArray, setPicsArray] = useState([]);
+  const [startFile, setStartFile] = useState(null);
+  // const [startFile, setStartFile] = useState(null);
+  const inputFile = useRef(null);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   console.log('useEffect ', user);
-  // }, []);
   function fnPics2Arr(e) {
-    console.log('EVENT FILE', e.target);
+    const preArray = [...picsArray];
+    const inputArray = Array.from(e.target.files);
+    console.log("EVENT FILE 111", inputArray);
+    inputArray.forEach((i) => preArray.push(i));
+    console.log("EVENT FILE 222", preArray);
+    setPicsArray(preArray);
+  }
+
+  function fnClickPlus() {
+    inputFile.current.click();
+  }
+
+  function fnOnDragStart(e, item, index) {
+    console.log(item, index);
+    // setStartFile(item);
+  }
+
+  function fnOnDragLeave(e) {
+    e.target.style.border = '3px solid white';
+  }
+
+  function fnOnDragEnd(e) {
+    // e.target.style.border = 'none';
+  }
+
+  function fnOnDragOver(e) {
+    e.preventDefault();
+    e.target.style.border = '3px solid red';
+  }
+
+  function fnOnDrop(e, item) {
+    e.preventDefault();
+    console.log(item);
+    e.target.style.border = '3px solid white';
+    // const tempArray = [...picsArray];
+    // const imageFrom = tempArray.splice();
   }
 
   return (
     <>
-      <div className="addhotel-preview">
-        <input type="file" onChange={fnPics2Arr}/>
-        {/* <div className="addhotel-pics-preview">1</div>
-        <div className="addhotel-pics-preview">2</div>
-        <div className="addhotel-pics-preview">1</div> */}
-        
-        <button className="addhotel-pics-btn">+</button>
+      <div>
+        <input
+          ref={inputFile}
+          type="file"
+          className="addhotel-input-file"
+          multiple
+          onChange={fnPics2Arr}
+        />
+        <div className="addhotel-preview">
+          {picsArray.length > 0 &&
+            picsArray.map((item, index) => (
+              <img
+                key={new Date() + Math.random()}
+                alt="not found"
+                className="addhotel-pics-preview"
+                src={URL.createObjectURL(item)}
+                draggable={true}
+                onDragStart={(e) => fnOnDragStart(e, item, index)}
+                onDragLeave={(e) => fnOnDragLeave(e)}
+                onDragEnd={(e) => fnOnDragEnd(e)}
+                onDragOver={(e) => fnOnDragOver(e)}
+                onDrop={(e) => fnOnDrop(e, item)}
+              />
+            ))}
+
+          <button className="addhotel-pics-btn" onClick={fnClickPlus}>
+            +
+          </button>
+        </div>
       </div>
     </>
   );
