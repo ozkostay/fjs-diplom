4a0f1402440368427075617898dfa200db87c423
ozkostay <ozkostay@mail.ru>
2024-01-13 08:35:19 +0500

    add chat modul

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HotelsModule } from './hotels/hotels.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { HotelsModule } from './hotels/hotels.module';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION),
     HotelsModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
