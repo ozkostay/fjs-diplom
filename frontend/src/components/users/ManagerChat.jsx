ede00710005106c90eee60dc827a0d1cdd6b7939
Konstantin <ozkostay@mail.ru>
2024-01-15 13:45:36 +0500

    backGround Li in managerChat

--- frontend/src/components/users/ManagerChat.jsx
+++ frontend/src/components/users/ManagerChat.jsx
@@ -4,12 +4,41 @@ import { useNavigate } from "react-router-dom";
 
 export default function ManagerChat() {
   const { user } = useSelector((state) => state.crUser);
+  const [seletedLi, setSeletedLi] = useState(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // let prevLi = useRef(null);
 
-  function fnUserLiHendler(e) {
+  function fnLiOnClick(e) {
     e.preventDefault();
+    if (seletedLi) {
+      seletedLi.style.backgroundColor = "white";
+      seletedLi.firstChild.style.color = "black";
+    }
+    const currentLi = e.target.closest(".mchat-users-cell");
+    setSeletedLi(currentLi);
+    currentLi.style.backgroundColor = "#5181b8";
+    currentLi.firstChild.style.color = "white";
+  }
+
+  function fnOnMouseOver(e) {
+    e.preventDefault();
+    const currentLi = e.target.closest(".mchat-users-cell");
+    if (currentLi === seletedLi) {
+      return;
+    }
+    // console.log("OVER", e.target.closest(".mchat-users-cell"));
+    currentLi.style.backgroundColor = "#cfd2da";
+    currentLi.firstChild.style.color = "black";
+  }
+
+  function fnOnMouseLeave(e) {
+    e.preventDefault();
+    const currentLi = e.target.closest(".mchat-users-cell");
+    if (currentLi === seletedLi) {
+      return;
+    }
+    // console.log("LEAVE  ", currentLi);
+    currentLi.style.backgroundColor = "white";
   }
 
   return (
@@ -25,31 +54,39 @@ export default function ManagerChat() {
               <ul className="">
                 <li
                   className="mchat-users-cell"
-                  onClick={(e) => fnUserLiHendler(e)}
+                  onClick={(e) => fnLiOnClick(e)}
+                  onMouseOver={(e) => fnOnMouseOver(e)}
+                  onMouseLeave={(e) => fnOnMouseLeave(e)}
                 >
-                  <div className="mchat-name">Иванов</div>
-                  <div className="mchat-signal">.</div>
+                  <span className="mchat-name">Иванов</span>
+                  <div className="mchat-signal"> </div>
                 </li>
                 <li
                   className="mchat-users-cell"
-                  onClick={(e) => fnUserLiHendler(e)}
+                  onClick={(e) => fnLiOnClick(e)}
+                  onMouseOver={(e) => fnOnMouseOver(e)}
+                  onMouseLeave={(e) => fnOnMouseLeave(e)}
                 >
                   <div className="mchat-name">Петров</div>
-                  <div className="mchat-signal">.</div>
+                  <div className="mchat-signal"> </div>
                 </li>
                 <li
                   className="mchat-users-cell"
-                  onClick={(e) => fnUserLiHendler(e)}
+                  onClick={(e) => fnLiOnClick(e)}
+                  onMouseOver={(e) => fnOnMouseOver(e)}
+                  onMouseLeave={(e) => fnOnMouseLeave(e)}
                 >
                   <div className="mchat-name">Сидоров</div>
-                  <div className="mchat-signal">.</div>
+                  <div className="mchat-signal"> </div>
                 </li>
                 <li
                   className="mchat-users-cell"
-                  onClick={(e) => fnUserLiHendler(e)}
+                  onClick={(e) => fnLiOnClick(e)}
+                  onMouseOver={(e) => fnOnMouseOver(e)}
+                  onMouseLeave={(e) => fnOnMouseLeave(e)}
                 >
                   <div className="mchat-name">Третьякова</div>
-                  <div className="mchat-signal">.</div>
+                  <div className="mchat-signal"> </div>
                 </li>
               </ul>
             </div>
